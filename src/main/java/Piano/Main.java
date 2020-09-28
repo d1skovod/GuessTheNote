57629dc5671ae8243a5eede42d619eaa55d0c61f
BuildTools <unconfigured@null.spigotmc.org>
2020-09-28 22:34:38 +0300

    test

---
+++ src/main/java/Piano/Main.java
@@ -0,0 +1,22 @@
+package Piano;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("View.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("GuessTheNote");
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/Piano/Model.java
@@ -0,0 +1,4 @@
+package Piano;
+
+public class Model {
+}
